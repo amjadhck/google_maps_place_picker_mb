f4d07bfef8cf3277a1786ab383bd7edcb2bcf782
Amjadh CK <32018699+amjadhck@users.noreply.github.com>
2023-05-04 18:35:46 +0400

    Auto complete text color changed

--- lib/src/components/prediction_tile.dart
+++ lib/src/components/prediction_tile.dart
@@ -26,7 +26,9 @@ class PredictionTile extends StatelessWidget {
 
   List<TextSpan> _buildPredictionText(BuildContext context) {
     final List<TextSpan> result = <TextSpan>[];
-    final textColor = Theme.of(context).textTheme.bodyText2!.color;
+     // edited below line
+    //final textColor = Theme.of(context).textTheme.bodyText2!.color;
+    final textColor = Colors.black;
 
     if (prediction.matchedSubstrings.length > 0) {
       MatchedSubstring matchedSubString = prediction.matchedSubstrings[0];
